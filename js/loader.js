4030431636f3a5210c2738876a5449c082651e7d
Dune Zerna <duzer17@student.sdu.dk>
2018-05-01 11:01:37 +0200

    Master 1.04
    Fixed Arrangement
    Fixed Kontakt
    Fixed Idrætscenter
    Fixed Bestilling
    Fixed Menu

--- js/loader.js
+++ js/loader.js
@@ -7,7 +7,7 @@
         $(".navbar").load("slideshow.html");
         $(".content_master").load("submenu.html");
     });
-    
+
     function loadMainMenu() {
         $(".content_sub > div").remove();
         $(".submenu_master").load("navbar_simple.html");
@@ -35,4 +35,26 @@
     function loadSnacks() {
         $(".content_sub > div").remove();
         $(".content_sub").load("KunKage.html");
+    }
+
+    function loadOrderFood() {
+        $(".content_sub > div").remove();
+        $(".navbar > div").remove();
+        $("div.content_master > center").remove();
+        $(".content_sub").load("webs.prototype/bestilling.html");
+    }
+
+    function loadEvents() {
+        $(".content_sub > div").remove();
+        $(".navbar > div").remove();
+        $("div.content_master > center").remove();
+        $(".navbar").load("video_sport.html")
+        $(".content_sub").load("arrangementer.html");
+    }
+
+    function loadSportsCenter() {
+        $(".content_sub > div").remove();
+        $(".navbar > div").remove();
+        $("div.content_master > center").remove();
+        $(".content_sub").load("idrætscenteret.html");
     }
